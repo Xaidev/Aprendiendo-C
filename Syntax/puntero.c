6e9655b924bb9e31f995d9118b3bfdd2cb6ae65b
Xaidev <neclxas3311@gmail.com>
2023-10-12 18:14:26 -0300

    121023

--- Syntax/puntero.c
+++ Syntax/puntero.c
@@ -2,11 +2,32 @@
     Puntero en c, son la direccion que tendremos en memoria
 
 */
+// int main()
+// {
+//     int a = 2;
+//     // con * podemos declarar un puntero
+//     int *apt = &a;
+//     printf("%p", apt);
+//     return 0;
+// }
+
+#include <stdio.h>
 int main()
 {
-    int a = 2;
-    // con * podemos declarar un puntero
-    int *apt = &a;
-    printf("%p", apt);
+    int matriz[2][3];
+    int *pmatriz = matriz[0];
+
+    for (int i = 0; i < sizeof(matriz) / sizeof(int); i++)
+    {
+        printf("\nDato %d: ", i + 1);
+        scanf("%d", pmatriz);
+        pmatriz++;
+    }
+    pmatriz = matriz[0];
+
+    for (int i = 0; i < sizeof(matriz) / sizeof(int); i++)
+    {
+        printf("\n%d", *(pmatriz + i));
+    }
     return 0;
 }

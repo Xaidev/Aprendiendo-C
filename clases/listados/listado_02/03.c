dd899cd652eeb4ffbb5f25bcf350ceba8a91f4bf
Xaidev <neclxas3311@gmail.com>
2023-10-12 22:07:02 -0300

    121023

---
+++ clases/listados/listado_02/03.c
@@ -0,0 +1,2 @@
+// Escriba un programa que pida un entero negativo x y que genere su representación complemento a dos
+// usando operaciones a nivel de bits (~, &, |, ^, << o >>). Su programa debe verificar que el número ingresado sea negativo.
